64a826c6a3baf0f727c8fdba6a28168e5c09551e
KrithikaV11 <krithi.ramya@gmail.com>
2014-06-10 14:19:35 +0530

    Employee App files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmployeeApp::Application.config.secret_token = '42640d3f6b1a4b5c706f217d4d42ab422eb51473f584a4a35374d1d3585e87df37aabd369960429b4cdbd6bf9b3fd6199e5b81296f2264bcd9065f6e6ee4ad5c'
